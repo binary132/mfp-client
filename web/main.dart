6e74ed0963f60f3a5b027bd11d4c856fde3192ad
Bodie Solomon <binary132@gmail.com>
2015-12-14 07:29:36 -0500

    Tweak license and comments

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,5 @@
-// Copyright (c) 2015 SynapseGarden. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2015 SynapseGarden. All rights reserved.
+// This code is licensed under BSD version 3.
 
 import 'dart:html';
 
